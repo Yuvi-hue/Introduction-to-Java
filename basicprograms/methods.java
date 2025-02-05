1d9da1e7b8bea491cfd547c3a093c4b0f2afea7a
Yuvi-hue <yuvraj.0510x@gmail.com>
2025-02-05 04:45:37 +0000

    Added methods.java

---
+++ basicprograms/methods.java
@@ -0,0 +1,8 @@
+public class methods {
+    public static void main(String[] args) {
+        myName("Yuvraj");
+    }
+    private static void myName(String name) {
+        System.out.println("My name is "+name);
+    }
+}
